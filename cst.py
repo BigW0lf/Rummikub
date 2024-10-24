78a808d92fe873e1dc0e892c887707d1f83476df
BigW0lf <130743820+BigW0lf@users.noreply.github.com>
2024-10-24 16:46:25 +0200

    Add files via upload

--- cst.py
+++ cst.py
@@ -4,7 +4,11 @@ Created on Thu Oct  3 15:39:47 2024
 
 @author: Formation
 """
+import numpy as np
 
-
-colors = ["bleu","orange","rouge", "noir"]
+true_colors = { 1 : "bleu",
+               2 : "orange",
+               3 : "rouge",
+               4 : "noir"}
 nombres = [0, 1, 2, 3, 4 ,5, 6, 7, 8, 9, 10, 11, 12, 13]
+
--- jeu.py
+++ jeu.py
@@ -10,12 +10,12 @@ import numpy as np
 
 
 class Tuile:
-    def __init__(self, nombre : int, color : str):
-        if not isinstance(color, str):
-            raise ValueError("Le paramètre 'color' doit être une chaîne de caractères.")
+    def __init__(self, nombre : int, color : int):
+        if not isinstance(color, int ):
+            raise ValueError("Le paramètre 'color' doit être un nombre.")
         if not isinstance(nombre, int):
             raise ValueError("Le paramètre 'nombre' doit être un nombre")
-        if nombre not in cst.nombres or color not in cst.colors or nombre == 0 and color in ["bleu", "orange"]:
+        if nombre not in cst.nombres or color not in cst.true_colors.keys() or nombre == 0 and color in [1, 2]:
             raise ValueError("La tuile n'est pas dans le jeu")
         self.nombre = nombre
         self.color = color
@@ -24,19 +24,23 @@ class Tuile:
     
     
     def isJoker(self):
-        return self.nombre == 0 and self.color in ["noir", "rouge"]
+        if self.nombre == 0 and self.color in [3, 4]:
+            return True
+        else: 
+            return False
     
     def __str__(self):
         if self.isJoker():
-            return f"Joker {self.color}"
+            return f"Joker {cst.true_colors[self.color]}"
         else:
-            return f"{self.nombre} {self.color}"
-        
+            return f"{self.nombre} {cst.true_colors[self.color]}"
+    def __eq__(self, o):
+        return self.nombre == o.nombre and self.color == o.color
 
 class Set:
-    def __init__(self):
-        self.tuiles = []
-    
+    def __init__(self, tuiles : list):
+        self.tuiles = tuiles
+        
     def ajout_tuile(self, tuile:Tuile):
         self.tuiles.append(tuile)
     
@@ -45,7 +49,71 @@ class Set:
             raise ValueError("la tuile n'est pas dans le set")       
         else:
             self.tuiles.remove(tuile)
-    def 
+    def validate (self):
+        colors = []
+        nbs = []
+        tuiles_array = np.array(self.tuiles)
+        for i in range (len(self.tuiles)):
+            dup_tuiles = self.tuiles
+            dup_tuiles.pop(i)
+            for j in range (len(self.tuiles)):
+                if self.tuiles[i] == dup_tuiles[j]:
+                    return False
+                else:
+                    next
+                
+        for t in self.tuiles:
+            nbs.append(t.nombre)
+            colors.append(t.color)
+            
+        Min = nbs[0]
+        Max = max(nbs)
+        taille = len(self.tuiles)
+        print(nbs, colors, Min, Max, taille)
+        
+        if Min == 0 or Max ==0 :
+            return False
+        if Min < Max :
+            verif_suite = np.array(cst.nombres[Min:Max+1]) - np.array(nbs)
+            check_suite = np.where(verif_suite != 0)
+            if taille < 3 :      
+                return False
+                
+            if len(check_suite) > 2 :
+                return False
+            if not check_suite :
+                return True
+            else :
+                
+                joker_check  = tuiles_array[check_suite]
+                for el in joker_check:
+                   val =  el.isJoker()
+                return val
+               
+        if Min == Max :
+            compte_colors = np.unique(colors,return_counts = True)
+            
+            print(compte_colors)
+            if 2 > taille > 4 :
+                return False
+            
+            verif_meme =  np.ones(taille) * Min - nbs 
+            check_meme = np.where(verif_meme != 0)
+            
+            if len(check_meme) > 2 :
+                return False
+            if not check_meme :
+                return True
+            else :
+                joker_check  = tuiles_array[check_meme]
+                for el in joker_check:
+                   val =  el.isJoker()
+                return val
+                
+            
+
+            
+        
     def __str__(self):
         if not self.tuiles:
             return "Le set est vide"
@@ -55,9 +123,9 @@ class Set:
 class Pioche:
     def __init__(self):
         self.pioche = []
-        for c in cst.colors:
+        for c in cst.true_colors.keys():
             for n in cst.nombres:
-                if not (n == 0 and c in ["bleu", "orange"]):
+                if not (n == 0 and c in [1,2]):
                     self.pioche.append(Tuile(n,c))
     def tirer(self):
         if not self.pioche:
@@ -80,7 +148,7 @@ class Table:
     def ajout_set (self, tuiles: Set):
         self.table.appends(tuiles)
     
-    def check_table(self, table : Table):
+    def check_table(self, table):
         for s in self.table:
             for t in self.table.Set:
                 check1 += len(t)
@@ -89,24 +157,26 @@ class Table:
                 check2 += len(t)
         return check1-check2
     
-    def validate
+
         
     
-tuile3 = Tuile(0, 'rouge' )
+tuile3 = Tuile(0, 3)
 
-set_de_tuiles = Set()
+tuile4 = Tuile(0, 4)
+
 
 # Création et ajout de tuiles
-tuile1 = Tuile(12, "rouge")
-set_de_tuiles.ajout_tuile(tuile1)
+tuile1 = Tuile(12, 3)
 
-tuile2 = Tuile(13, "noir")
-set_de_tuiles.ajout_tuile(tuile2)
 
-set_de_tuiles.ajout_tuile(tuile3)
+tuile2 = Tuile(12, 3)
+
+tuile5 = Tuile(8, 3)
+
 
 pioche = Pioche()
 
+set1 = Set([tuile2,tuile3,tuile1])
 
-print(pioche.tirer())
-print(pioche)
+print(set1)
+print(set1.validate())
